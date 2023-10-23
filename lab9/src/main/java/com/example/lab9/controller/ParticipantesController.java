670f937161548441b7826afd083da6e5714bb271
Oscar <a20193315@pucp.edu.pe>
2023-10-23 16:52:19 -0500

    avance 2

--- lab9/src/main/java/com/example/lab9/controller/ParticipantesController.java
+++ lab9/src/main/java/com/example/lab9/controller/ParticipantesController.java
@@ -3,8 +3,10 @@ package com.example.lab9.controller;
 import com.example.lab9.entity.Equipo;
 import com.example.lab9.entity.Participante;
 import com.example.lab9.repository.ParticipanteRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -40,6 +42,23 @@ public class ParticipantesController {
         }
         responseJson.put("estado", "creado");
         return ResponseEntity.status(HttpStatus.CREATED).body(responseJson);
+    }
+
+
+
+
+
+
+
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<HashMap<String, String>> gestionException(HttpServletRequest request) {
+        HashMap<String, String> responseMap = new HashMap<>();
+        if (request.getMethod().equals("POST") || request.getMethod().equals("PUT")) {
+            responseMap.put("estado", "error");
+            responseMap.put("msg", "Debe enviar un producto");
+        }
+        return ResponseEntity.badRequest().body(responseMap);
     }
 
 }
