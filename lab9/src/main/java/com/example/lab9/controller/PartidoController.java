e8538d7c589ff9c48c8201071c457c81c2fe87c2
Oscar <a20193315@pucp.edu.pe>
2023-10-23 17:58:16 -0500

    avance 2a

--- lab9/src/main/java/com/example/lab9/controller/PartidoController.java
+++ lab9/src/main/java/com/example/lab9/controller/PartidoController.java
@@ -1,7 +1,9 @@
 package com.example.lab9.controller;
 
-import com.example.lab9.entity.Participante;
+
+import com.example.lab9.entity.Historialpartido;
 import com.example.lab9.entity.Partido;
+import com.example.lab9.repository.HistorialpartidoRepository;
 import com.example.lab9.repository.PartidoRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,46 @@ import java.util.List;
 @RestController
 @RequestMapping("/partido")
 public class PartidoController {
+
+    final PartidoRepository partidoRepository;
+    final HistorialpartidoRepository historialPartidoRepository;
+
+    public PartidoController(PartidoRepository partidoRepository, HistorialpartidoRepository historialPartidoRepository) {
+        this.partidoRepository = partidoRepository;
+
+        this.historialPartidoRepository = historialPartidoRepository;
+    }
+
+
+    @GetMapping(value = {"/", ""})
+    public List<Partido> lista() {
+        return partidoRepository.findAll();
+    }
+
+    //crear
+    @PostMapping({"/registro","/registro/"})
+    public ResponseEntity<HashMap<String, Object>> guardarPartido(
+            @RequestBody Partido partido,
+            @RequestParam(value = "fetchId", required = false) boolean fetchId) {
+
+        HashMap<String, Object> responseJson = new HashMap<>();
+
+        partidoRepository.save(partido);
+        if (fetchId) {
+            responseJson.put("id", partido.getId());
+        }
+        responseJson.put("estado", "creado");
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseJson);
+    }
+
+
+
+    // Listado de historial de productos
+    @GetMapping(value = {"/gethistorialpartidos"})
+    public List<Historialpartido> listaPartidos() {
+        return historialPartidoRepository.findAll();
+    }
+
 
 
 
